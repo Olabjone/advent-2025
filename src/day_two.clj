c679474354678049decbfb2e97b20bf91cfc4208
Olabjornfot <104162070+Olabjone@users.noreply.github.com>
2025-12-10 16:51:28 +0100

    PART two was rougher

--- src/day_two.clj
+++ src/day_two.clj
@@ -2,10 +2,31 @@
       (:require [input]))
 
 
+
+;; PART 1 invalid
+(defn old-invalid-id? [id]
+  (let [length (count id)
+        middle (/ length 2)]
+    (if (even? length) (= (subs id 0 middle) (subs id middle length)) false)))
+
+;;PART 2 invalid
 (defn invalid-id? [id]
-      (let [length (count id)
-            middle (/ length 2)]
-            (if (even? length) (= (subs id 0 middle) (subs id middle length)) false)))
+  (let [length (count id)
+        middle (/ length 2)]
+    (loop [blocksize 1]
+      (if (> blocksize middle)
+        false)
+      (let [block (subs id 0 blocksize)]
+        (if (every?
+            (fn [i]
+              (= (nth id i)
+                 (nth block (mod i blocksize))))
+                  (range length))
+        true
+        (recur (inc blocksize)))))))
+
+(defn invalid-id? [id]
+  (boolean (re-matches #"(\d+)\1+" id)))
 
 ;;Returns a lazy sequence of all nums from a to b(inclusive)
 (defn parse-range [range-str]
@@ -28,5 +49,4 @@
 (def totalofInvalids
       (reduce + (map #(Long/parseLong %) invalids)))
 (println "Total invalid IDs:" total-invalids)
-
 (println totalofInvalids)
